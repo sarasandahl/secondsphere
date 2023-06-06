4eafa51bf77e46b85c32bc275ac9040351cbb558
sarasandahl <92533105+sarasandahl@users.noreply.github.com>
2023-06-06 09:57:25 +0200

    ny funktion plus bild i varor hos säljare

--- classes/views/test.php
+++ classes/views/test.php
@@ -1,42 +1,4 @@
 <?php
-	//funkar ej
-class UsersItemView {
-    public function renderSingleUsersItems(array $items):void {	
-	$keys = array_keys($items);
-
-	for($i = 0; $i < count($item); $i++) {
-        $item = explode(" : ", $item);
-		echo "<br>";
-		foreach($item[$keys[$i]] as $key => $value) {
-		    echo $value . "<br>";
-			}
-			echo "<br>";
-		}
-	}
-}
-
-////////////////////////////////////////////////////////////////////
-
-//funkar ej
-class UsersItemView {
-    public function renderSingleUsersItems(array $items):void {
-
-echo "<ul>";
-        foreach($items as $item) {
-            foreach($item['product_name'] as $i) {
-                echo "<li>{$i}</li>";            
-            }
-
-        }
-        echo "</ul>";
-
-        foreach($items['image'] as $image) {
-            echo "<img src='{$image}' alt='image' height='600px'><br>";
-        }
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////
 
 //1.
 class UsersItemView {
@@ -70,6 +32,28 @@ class UsersItemView {
 
 ////////////////////////////////////////////////////////////////////////////
 
+//3.
+    class UsersItemView {
+        public function renderSingleUsersItems(array $items):void {
+    
+        foreach($items as $item) {
+        ?>
+            <div>
+            <h4><?= $item["product_name"] ?></h4>
+
+            <?php
+            echo "<img src='{$item['image']}' alt='image' height='600px'><br>";
+
+            ?>
+            <p>Pris: <?= $item["price"] ?></p>
+        </div>
+        <?php
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////
+
 //funkar ej
 class UsersItemView {
     public function renderSingleUsersItems(array $items):void {
@@ -90,3 +74,40 @@ echo "<ul>";
 
 
 ////////////////////////////////////////////////////////////////////////////
+
+	//funkar ej
+	class UsersItemView {
+		public function renderSingleUsersItems(array $items):void {	
+		$keys = array_keys($items);
+	
+		for($i = 0; $i < count($item); $i++) {
+			$item = explode(" : ", $item);
+			echo "<br>";
+			foreach($item[$keys[$i]] as $key => $value) {
+				echo $value . "<br>";
+				}
+				echo "<br>";
+			}
+		}
+	}
+	
+////////////////////////////////////////////////////////////////////
+	
+	//funkar ej
+	class UsersItemView {
+		public function renderSingleUsersItems(array $items):void {
+	
+	echo "<ul>";
+			foreach($items as $item) {
+				foreach($item['product_name'] as $i) {
+					echo "<li>{$i}</li>";            
+				}
+	
+			}
+			echo "</ul>";
+	
+			foreach($items['image'] as $image) {
+				echo "<img src='{$image}' alt='image' height='600px'><br>";
+			}
+		}
+	}
--- classes/views/users-item-view.php
+++ classes/views/users-item-view.php
@@ -3,11 +3,19 @@
     class UsersItemView {
         public function renderSingleUsersItems(array $items):void {
     
-            foreach($items as $item) {
+        foreach($items as $item) {
+        ?>
+            <div>
+            <h4><?= $item["product_name"] ?></h4>
 
-                foreach($item as $key => $value) { 
-                echo $value . "<br>"; 
-            }
+            <?php
+            echo "<img src='{$item['image']}' alt='image' height='600px'><br>";
+
+            ?>
+            <p>Pris: <?= $item["price"] ?></p>
+            <p>Finns i lager?: <?= $item["sold"] ?></p>
+        </div>
+        <?php
         }
-    }	
+    }
 }
